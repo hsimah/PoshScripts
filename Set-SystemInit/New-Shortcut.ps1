6d6d9c25342a937b8cf6b2deabd47d488beaec65
Hamish Blake <Hamish Blake>
2016-03-26 17:28:31 +1000

    New-Shortcut WIP

---
+++ Set-SystemInit/New-Shortcut.ps1
@@ -0,0 +1,24 @@
+﻿$Target = '%windir%\System32\cmd.exe'
+
+function New-Shortcut([string]$Target, [string]$Arguments, [string]$Path) {
+    $WshShell = New-Object -comObject WScript.Shell
+    $Shortcut = $WshShell.CreateShortcut($Path)
+    $Shortcut.TargetPath = $Target
+    $Shortcut.Arguments = $Arguments
+    $Shortcut.Save()
+}
+
+New-Shortcut -Target $Target -Path "C:\Users\hamis\Desktop\test.lnk" -Arguments '/c "title Start-DevEnv & powershell.exe %CMDLET_REPO%\Start-DevEnv\Start-DevEnv.ps1"'
+
+$f = 'C:\Users\hamis\Desktop'
+$n = 'test.lnk'
+
+#$shell = new-object -com "Shell.Application"  
+#$folder = $shell.Namespace($f)    
+#$item = $folder.Parsename($n)
+#$verb = $item.Verbs() | ? {$_.Name -eq 'Pin to Tas&kbar'}
+#if ($verb) {$verb.DoIt()}
+
+$sa = new-object -c shell.application
+$pn = $sa.namespace($f).parsename($n)
+$pn.invokeverb('taskbarpin')
