71016d249cf7dec5a0ffe1824f041cea5fd8696c
Hamish Blake <Hamish Blake>
2016-03-26 18:40:16 +1000

    Fixed #3

--- Set-SystemInit/New-Shortcut.ps1
+++ Set-SystemInit/New-Shortcut.ps1
@@ -1,17 +1,39 @@
-﻿$Target = '%windir%\System32\cmd.exe'
+﻿param(
+    [string]$Target,
+    [string]$Arguments,
+    [string]$Path,
+    [string]$IconLocation,
+    [int]$IconIndex
 
-function New-Shortcut([string]$Target, [string]$Arguments, [string]$Path) {
-    $WshShell = New-Object -comObject WScript.Shell
-    $Shortcut = $WshShell.CreateShortcut($Path)
-    $Shortcut.TargetPath = $Target
-    $Shortcut.Arguments = $Arguments
+)
+
+$Shell = New-Object -ComObject WScript.Shell
+$Shortcut = $Shell.CreateShortcut($Path)
+
+$Shortcut.TargetPath = $Target
+$Shortcut.Arguments = $Arguments
+$Shortcut.Save()
+
+# Set shortcut icon
+if ($IconLocation -and $IconIndex) {
+    $Folder = Split-Path -Path $Path -Parent
+    $FileName = Split-Path -Path $Path -Leaf
+
+    $Shell = New-Object -Com Shell.Application
+    $Shortcut = (($Shell.NameSpace($Folder)).ParseName($FileName)).GetLink
+
+    $Shortcut.SetIconLocation($IconLocation, $IconIndex)
     $Shortcut.Save()
 }
 
-New-Shortcut -Target $Target -Path "C:\Users\hamis\Desktop\test.lnk" -Arguments '/c "title Start-DevEnv & powershell.exe %CMDLET_REPO%\Start-DevEnv\Start-DevEnv.ps1"'
 
-$f = 'C:\Users\hamis\Desktop'
-$n = 'test.lnk'
+
+## TODO: pin shortcut
+## add pin parameter
+
+
+#$f = 'C:\Users\hamis\Desktop'
+#$n = 'test.lnk'
 
 #$shell = new-object -com "Shell.Application"  
 #$folder = $shell.Namespace($f)    
@@ -19,8 +41,8 @@ $n = 'test.lnk'
 #$verb = $item.Verbs() | ? {$_.Name -eq 'Pin to Tas&kbar'}
 #if ($verb) {$verb.DoIt()}
 
-$sa = new-object -c shell.application
-$pn = $sa.namespace($f).parsename($n)
-$pn.invokeverb('taskbarpin')
+#$sa = new-object -c shell.application
+#$pn = $sa.namespace($f).parsename($n)
+#$pn.invokeverb('taskbarpin')
 
 # this is non-working as Microsoft have removed the ability to programmatically pin items
