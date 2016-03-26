ca003838ffc5987612f53bd5f80b30d1fa94debf
Hamish Blake <Hamish Blake>
2016-03-26 16:52:22 +1000

    Fixes #2

---
+++ Set-SystemInit/Set-SystemInit.ps1
@@ -0,0 +1,4 @@
+﻿$Path = Split-Path $MyInvocation.MyCommand.Path -Parent
+
+# PowerShell cmdlet repository location
+. $Path\New-UserEnvironmentalVariable.ps1 -Key "PSLocalModulePath" -Value "%USERPROFILE%\OneDrive\Development\Scripts\PowerShell"
