3259df92e5bdd45170f6cf88e953275ec174eb43
Hamish Blake <Hamish Blake>
2016-03-26 16:52:32 +1000

    Fixes #1

---
+++ Set-SystemInit/New-UserEnvironmentalVariable.ps1
@@ -0,0 +1,10 @@
+﻿param(
+    [Parameter(Mandatory=$true, Position=1)]
+    [string]$Key,
+    [Parameter(Mandatory=$true, Position=2)]
+    [string]$Value
+)
+
+if ((Get-ChildItem env: | Where-Object { $_.Name -eq $Key }).Length -eq 0) {
+    Set-ItemProperty -Path HKCU:\Environment -Name $Key -Value $Value -Type ExpandString
+}
