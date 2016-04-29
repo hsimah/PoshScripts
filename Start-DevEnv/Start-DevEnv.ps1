f60e8cb40f3552e19e2040706aed05e37546781b
Hamish Blake <hamishblake@outlook.com>
2016-04-29 20:22:33 +1000

    Fixes #13 adds additional prompt to restart VS/shut down dev env; scaffolded for additional services/user inputs

--- Start-DevEnv/Start-DevEnv.ps1
+++ Start-DevEnv/Start-DevEnv.ps1
@@ -1,28 +1,76 @@
-﻿$Name = 'MSSQL$SQLEXPRESS'
-$Service = Get-Service -Name $Name
+﻿function Show-ConsoleMenu([Hashtable]$Options, [string]$Title, [string]$Message) {
+    $Choices = @()
+    foreach ($Option in $Options.GetEnumerator()) {
 
-if ($Service.Status -ne 'Running') {
-    Write-Output "Starting $Name..."
-    Start-Service -InputObject $Service -ErrorAction Stop
+        $Choices += New-Object System.Management.Automation.Host.ChoiceDescription $("&{0}" -f $Option.Name), $Option.Value
+    }
+    
+    $Result = $host.ui.PromptForChoice($Title, $Message, $Choices, 0)
+
+    return $Result
 }
 
-Write-Output "$Name is $($Service.Status)."
+function Invoke-VisualStudio() {
+    Write-Output "Starting Visual Studio 2015 Community..."
 
-Write-Output "Starting Visual Studio 2015 Community..."
+    $Process = Start-Process -FilePath "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" -PassThru
 
-$Process = Start-Process -FilePath "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" -PassThru
+    Write-Output "Visual Studio 2015 Community running."
 
-Write-Output "Visual Studio 2015 Community running."
+    Wait-Process -InputObject $Process
 
-Wait-Process -InputObject $Process
+    Write-Output "Visual Studio 2015 Community closed."
+}
 
-Write-Output "Visual Studio 2015 Community closed."
+function Start-DependentService([string]$Name) {
+    $Service = Get-Service -Name $Name
 
-$Outservice = Stop-Service -InputObject $Service -Confirm -PassThru
+    if ($Service.Status -ne 'Running') {
+        Write-Output "Starting $Name..."
+        Start-Service -InputObject $Service -ErrorAction Stop
+    }
 
-if ($Outservice) {
-    Write-Output "Stopping $Name..."
     Write-Output "$Name is $($Service.Status)."
-} else {
-    Write-Output "$Name is still $($Service.Status)."
+}
+
+function Stop-DependentService([string]$Name) {
+    $Service = Get-Service -Name $Name
+
+    if ($Service.Status -eq 'Running') {
+        Write-Output "Stopping $Name..."
+        Stop-Service -InputObject $Service -Confirm
+    }
+
+    Write-Output "$Name is $($Service.Status)."
+}
+
+$Services = @('MSSQL$SQLEXPRESS')
+
+# Start dependent services
+foreach ($Service in $Services) {
+    #Start-DependentService -Name $Service
+}
+
+while ($true) {
+    # Start Visual Studio
+    Invoke-VisualStudio
+
+    # Visual Studio closed - prompt user for next action
+    $Result = Show-ConsoleMenu -Title "Visual Studio closed" -Message "Action to take?" -Options @{ 
+        Restart="Restart Visual Studio 2015."; End="End development session (shut down SQL Server)."; }
+
+    switch ($Result) {
+        0 {
+            # stop dependent services
+            foreach ($Service in $Services) {
+                Stop-DependentService -Name $Service
+            }
+
+            exit
+        }
+        1 {
+            # restart Visual studio
+            continue
+        }
+    }
 }
