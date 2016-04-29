68e47337bdcfdb08bf310474deccb270183f7ab5
Hamish Blake <hamishblake@outlook.com>
2016-04-29 20:58:30 +1000

    Fixes #14 tests for an orchard error log file and when found starts a shell window to tail the file

--- Start-DevEnv/Start-DevEnv.ps1
+++ Start-DevEnv/Start-DevEnv.ps1
@@ -10,6 +10,21 @@
     return $Result
 }
 
+function Invoke-LogTail() {
+    $LogFileFormat = 'orchard-{0}-{1}.log'
+    $LogFileLocation = "$env:DEV\Uqcc\src\Orchard.Web\App_Data\Logs"
+    $Date = Get-Date -Format yyyy.MM.dd
+    $Configuration = 'debug','error'
+
+    $LogFilePath = "$LogFileLocation\$($LogFileFormat -f $Configuration[1],$Date)"
+
+    while (!$(Test-Path -Path $LogFilePath)) {
+        Start-Sleep -Seconds 120
+    }
+
+    Start-Process powershell -ArgumentList "`$host.ui.RawUI.WindowTitle = 'Error log file'; Get-Content -Path $LogFilePath -Wait"
+}
+
 function Invoke-VisualStudio() {
     Write-Output "Starting Visual Studio 2015 Community..."
 
@@ -48,8 +63,10 @@ $Services = @('MSSQL$SQLEXPRESS')
 
 # Start dependent services
 foreach ($Service in $Services) {
-    #Start-DependentService -Name $Service
+    Start-DependentService -Name $Service
 }
+
+Invoke-LogTail
 
 while ($true) {
     # Start Visual Studio
