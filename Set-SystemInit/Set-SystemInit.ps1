b873d992770ce8e4fab76f352f13fb597b601508
Hamish Blake <hamishblake@outlook.com>
2016-04-29 21:01:16 +1000

    Fixes #9 adds %DEV% system variable and creates folder in C:\dev

--- Set-SystemInit/Set-SystemInit.ps1
+++ Set-SystemInit/Set-SystemInit.ps1
@@ -3,5 +3,11 @@
 # PowerShell cmdlet repository location
 . $Path\New-UserEnvironmentalVariable.ps1 -Key 'PSLocalModulePath' -Value '%USERPROFILE%\OneDrive\Development\Scripts\PowerShell'
 
+# set up dev directory and env var
+if (!(Test-Path -Path C:\dev)) {
+    New-Item -Path C:\dev -ItemType directory
+}
+. $Path\New-UserEnvironmentalVariable.ps1 -Key 'DEV' -Value 'C:\dev'
+
 # Create shortcut for devenv cmdlet
 . $Path\New-Shortcut.ps1 -Target '%windir%\System32\cmd.exe' -Path 'C:\Users\hamis\Desktop\Start Development Environment.lnk' -Arguments '/c "title RUNNING_DevelopmentEnvironment & powershell.exe %PSLocalModulePath%\Start-DevEnv\Start-DevEnv.ps1"' -IconLocation 'C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe' -IconIndex 4
